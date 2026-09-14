9ad4bf9ce22ece8d92ba421b768179c4c44c8d44
Eazow <eazow@163.com>
2026-09-14 17:26:21 +0800

    Site updated: 2026-09-14 17:26:19

---
+++ js/schemes/muse.js
@@ -0,0 +1,44 @@
+/* global CONFIG */
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const isRight = CONFIG.sidebar.position === 'right';
+
+  const sidebarToggleMotion = {
+    init : function() {
+      document.querySelector('.sidebar-dimmer').addEventListener('click', this.clickHandler.bind(this));
+      document.querySelector('.sidebar-toggle').addEventListener('click', this.clickHandler.bind(this));
+      window.addEventListener('sidebar:show', this.showSidebar);
+      window.addEventListener('sidebar:hide', this.hideSidebar);
+    },
+    clickHandler: function() {
+      document.body.classList.contains('sidebar-active') ? this.hideSidebar() : this.showSidebar();
+    },
+    showSidebar: function() {
+      document.body.classList.add('sidebar-active');
+      const animateAction = isRight ? 'fadeInRight' : 'fadeInLeft';
+      document.querySelectorAll('.sidebar .animated').forEach((element, index) => {
+        element.style.animationDelay = (100 * index) + 'ms';
+        element.classList.remove(animateAction);
+        setTimeout(() => {
+          // Trigger a DOM reflow
+          element.classList.add(animateAction);
+        });
+      });
+    },
+    hideSidebar: function() {
+      document.body.classList.remove('sidebar-active');
+    }
+  };
+  if (CONFIG.sidebar.display !== 'remove') sidebarToggleMotion.init();
+
+  function updateFooterPosition() {
+    const footer = document.querySelector('.footer');
+    const containerHeight = document.querySelector('header.header').offsetHeight + document.querySelector('.main').offsetHeight + footer.offsetHeight;
+    footer.classList.toggle('footer-fixed', containerHeight <= window.innerHeight);
+  }
+
+  updateFooterPosition();
+  window.addEventListener('resize', updateFooterPosition);
+  window.addEventListener('scroll', updateFooterPosition, { passive: true });
+});
